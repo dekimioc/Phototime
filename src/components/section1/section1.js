c053d4579c142396dc5f0269c326901d9f235ac0
Dejan <dekimioc@yahoo.com>
2018-11-27 09:53:47 +0100

    Added and styled header

---
+++ src/components/section1/section1.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const section = () => (
+    <div>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+        <h1>AAAAAAAAAAAAAAAAAAAAAAAAAA</h1>
+    </div>
+);
+
+export default section;
