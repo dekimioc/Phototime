82b97a4550b3a56e722eb289441fff6ad62c5de9
Dejan Miocinovic <dekimioc@yahoo.com>
2020-02-27 23:53:50 +0100

    Updates

---
+++ precache-manifest.68cbc00df270b14070e744f5c60e4ae2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee51b03a40eeff1ea2f3",
+    "url": "/Phototime/static/css/main.3237f3f6.chunk.css"
+  },
+  {
+    "revision": "ee51b03a40eeff1ea2f3",
+    "url": "/Phototime/static/js/main.ee51b03a.chunk.js"
+  },
+  {
+    "revision": "8644d2db4371b04b9363",
+    "url": "/Phototime/static/js/1.8644d2db.chunk.js"
+  },
+  {
+    "revision": "40573930c4ae452dd378",
+    "url": "/Phototime/static/js/runtime~main.40573930.js"
+  },
+  {
+    "revision": "cf82ff820ccc95756af951858ee5aa8d",
+    "url": "/Phototime/static/media/header_back.cf82ff82.jpg"
+  },
+  {
+    "revision": "e1d5f0d85a7130c9a24865cff253bcf2",
+    "url": "/Phototime/static/media/subscribe.e1d5f0d8.jpg"
+  },
+  {
+    "revision": "49f2670065db396a5f529f3437756b57",
+    "url": "/Phototime/static/media/logo-green.49f26700.png"
+  },
+  {
+    "revision": "2519c2faf1b6523a4fe21eaf3f19bc2e",
+    "url": "/Phototime/static/media/hand_half_1.2519c2fa.png"
+  },
+  {
+    "revision": "0cd96f95fb3ecb68dbddeb0c56524a15",
+    "url": "/Phototime/static/media/iphone.0cd96f95.jpg"
+  },
+  {
+    "revision": "58610930a53a5c7e4347c02dfd5f8095",
+    "url": "/Phototime/static/media/van.58610930.jpg"
+  },
+  {
+    "revision": "ede29f5061f803984eef43f724ba4060",
+    "url": "/Phototime/static/media/autumn.ede29f50.png"
+  },
+  {
+    "revision": "9253429d4f4af42942392ee979e0f26c",
+    "url": "/Phototime/index.html"
+  }
+];
